3bdf08cb597e841d49229ce91b2a00d992a35161
AShenoy16 <ashe194@aucklanduni.ac.nz>
2024-09-29 00:03:23 +1200

    create initial post request route

--- backend/main.py
+++ backend/main.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return "Home"
 
+@app.route("/create-transcription", methods =["POST"])
+def create_transcription():
+    data = request.get_json()
+    
+    return jsonify(data), 201
+
 if __name__ == "__main__":
     app.run(debug=True)
