0c026a16a3a52bfb1b66207d4d7b8f286f1443e9
AShenoy16 <ashe194@aucklanduni.ac.nz>
2024-10-03 21:04:51 +1300

    Created server hosting for frontend integration

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,21 @@
-from flask import Flask, request, jsonify 
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/")
 def home():
-    return "Home"
+    return jsonify(message="Home"), 200
 
-@app.route("/create-transcription", methods =["POST"])
+
+@app.route("/create-transcription", methods=["POST"])
 def create_transcription():
     data = request.get_json()
-    
+
     return jsonify(data), 201
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
